cb7a174463fb8e24a65043820f5a2f13ca0a8878
Jon Bell <github@lot23.com>
2022-11-07 20:38:07 +0000

    Testing cypress int

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,21 @@
-const { defineConfig } = require("cypress");
-const cypressReplay = require("@replayio/cypress");
+const { defineConfig } = require('cypress');
+const cypressReplay = require('@replayio/cypress');
 
 module.exports = defineConfig({
-  projectId: "y3akbv",
-  e2e: {
-    baseUrl: "http://localhost:3000",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      cypressReplay.default(on, config);
-    },
-  },
+	projectId: 'y3akbv',
+	e2e: {
+		baseUrl: 'http://localhost:3000',
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+			cypressReplay.default(on, config);
+			return config;
+		},
+	},
 
-  component: {
-    devServer: {
-      framework: "next",
-      bundler: "webpack",
-    },
-  },
+	component: {
+		devServer: {
+			framework: 'next',
+			bundler: 'webpack',
+		},
+	},
 });
