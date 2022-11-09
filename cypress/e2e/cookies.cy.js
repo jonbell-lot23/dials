5313d9c99cf88be91adddff3f86b46f4b8c136c0
Jon Bell <github@lot23.com>
2022-11-09 01:36:59 +0000

    Removing failure from equation

--- cypress/e2e/cookies.cy.js
+++ cypress/e2e/cookies.cy.js
@@ -3,6 +3,6 @@ describe('Check some stuff', () => {
 		cy.visit('https://bookshelf.lot23.com');
 	});
 	it('Loads bad website', () => {
-		cy.visit('https://bookshelf.lot25.com');
+		cy.visit('https://bookshelf.lot23.com');
 	});
 });
