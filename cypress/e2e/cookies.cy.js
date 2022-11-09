b39b25a9a015150218ec25d66d55a2e195583108
Jon Bell <github@lot23.com>
2022-11-09 01:40:32 +0000

    Removing failure

--- cypress/e2e/cookies.cy.js
+++ cypress/e2e/cookies.cy.js
@@ -2,7 +2,4 @@ describe('Check some stuff', () => {
 	it('Loads good website', () => {
 		cy.visit('https://bookshelf.lot23.com');
 	});
-	it('Loads bad website', () => {
-		cy.visit('https://bookshelf.lot23.com');
-	});
 });
